5befe351a811605a85119a0f3434cb8b3893de73
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2025-02-27 11:30:51 +0100

    add tests for adapter.constrain

--- tests/test_adapters/test_adapters.py
+++ tests/test_adapters/test_adapters.py
@@ -31,3 +31,42 @@ def test_serialize_deserialize(adapter, custom_objects, random_data):
     deserialized_processed = deserialized(random_data)
     for key, value in processed.items():
         assert np.allclose(value, deserialized_processed[key])
+
+
+def test_constrain():
+    import numpy as np
+    import warnings
+    from bayesflow.adapters import Adapter
+
+    data = {
+        "x1": np.random.exponential(1, size=(32, 1)),
+        "x2": -np.random.exponential(1, size=(32, 1)),
+        "x3": np.random.beta(0.5, 0.5, size=(32, 1)),
+        "x4": np.vstack((np.zeros(shape=(16, 1)), np.ones(shape=(16, 1)))),
+        "x5": np.zeros(shape=(32, 1)),
+        "x6": np.zeros(shape=(32, 1)),
+    }
+
+    adapter = (
+        Adapter()
+        .constrain("x1", lower=0)
+        .constrain("x2", upper=0)
+        .constrain("x3", lower=0, upper=1)
+        .constrain("x4", lower=0, upper=1, inclusive="both")
+        .constrain("x5", lower=0, inclusive="none")
+        .constrain("x6", upper=0, inclusive="none")
+    )
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", RuntimeWarning)
+        result = adapter(data)
+
+    # checks if transformations indeed have been applied
+    assert result["x1"].min() < 0.0
+    assert result["x2"].max() > 0.0
+    assert result["x3"].min() < 0.0
+    assert result["x3"].max() > 1.0
+    assert np.isfinite(result["x4"].min())
+    assert np.isfinite(result["x4"].max())
+    assert np.isneginf(result["x5"][0])
+    assert np.isinf(result["x6"][0])
