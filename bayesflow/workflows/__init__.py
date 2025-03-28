fcfbec0d6bfa403722551f4a6abcab1c235ee87e
LarsKue <lars@kuehmichel.de>
2025-03-28 11:26:32 +0100

    add workflows docs

--- bayesflow/workflows/__init__.py
+++ bayesflow/workflows/__init__.py
@@ -1,3 +1,6 @@
+"""
+Provides :py:class:`~bayesflow.workflows.BasicWorkflow`, a high-level interface for working on typical BayesFlow applications without having to worry about the internals.
+"""
 from .basic_workflow import BasicWorkflow
 
 from ..utils._docs import _add_imports_to_all
