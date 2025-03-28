9c242467dccd3ee35cf3593614a82c45d6e8469d
LarsKue <lars@kuehmichel.de>
2025-03-28 11:25:46 +0100

    add simulators docs

--- bayesflow/simulators/__init__.py
+++ bayesflow/simulators/__init__.py
@@ -1,3 +1,8 @@
+"""
+This module provides the :py:class:`~bayesflow.simulators.Simulator`, which is the base implementation of a generative mathematical model, data generating process.
+Its primary function is to sample data with the :py:meth:`~bayesflow.simulators.Simulator.sample` method.
+The module also contains several other kinds of Simulators, as well as the function :py:func:`~bayesflow.simulators.make_simulator` to facilitate easy implementation.
+"""
 from .sequential_simulator import SequentialSimulator
 from .hierarchical_simulator import HierarchicalSimulator
 from .lambda_simulator import LambdaSimulator
