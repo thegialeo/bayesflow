d05a8998ea82b06fd5cb827fa1ea9cfdf67977f9
LarsKue <lars@kuehmichel.de>
2025-04-07 20:10:31 -0400

    add jupyter notebook tests

---
+++ tests/utils/jupyter.py
@@ -0,0 +1,11 @@
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def run_notebook(path):
+    with open(str(path)) as f:
+        nb = nbformat.read(f, nbformat.NO_CONVERT)
+
+    kernel = ExecutePreprocessor(timeout=600, kernel_name="python3")
+
+    return kernel.preprocess(nb)
