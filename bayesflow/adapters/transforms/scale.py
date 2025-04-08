0ddaad52e92763d904079341e67234fa9aac4c7a
LarsKue <lars@kuehmichel.de>
2025-04-08 11:34:26 -0400

    make scale and shift serializable

--- bayesflow/adapters/transforms/scale.py
+++ bayesflow/adapters/transforms/scale.py
@@ -1,11 +1,24 @@
+from keras.saving import (
+    deserialize_keras_object as deserialize,
+    register_keras_serializable as serializable,
+    serialize_keras_object as serialize,
+)
 import numpy as np
 
 from .elementwise_transform import ElementwiseTransform
 
 
+@serializable(package="bayesflow.adapters")
 class Scale(ElementwiseTransform):
-    def __init__(self, scale: float | np.ndarray):
-        self.scale = scale
+    def __init__(self, scale: np.typing.ArrayLike):
+        self.scale = np.array(scale)
+
+    @classmethod
+    def from_config(cls, config: dict, custom_objects=None) -> "ElementwiseTransform":
+        return cls(scale=deserialize(config["scale"]))
+
+    def get_config(self) -> dict:
+        return {"scale": serialize(self.scale)}
 
     def forward(self, data: np.ndarray, **kwargs) -> np.ndarray:
         return data * self.scale
